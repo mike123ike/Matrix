9897c9bdb6c73563f8ec55c7ad37119d1eb764db
mike123ike <mike123.ike@gmail.com>
2024-10-07 07:52:44 -0500

    Add exponent method

--- matrix.py
+++ matrix.py
@@ -1,11 +1,12 @@
 class Matrix:
-    def __init__(self, columns=None, rows=None):
+    def __init__(self, rows=None, columns=None):
         if columns != None:
             self.columns = columns
             self.rows = rows
             self.matrix = [[0 for column in range(self.columns)] for row in range(self.rows)]
         else:
             self.get_matrix()
+   
     def get_matrix(self):
         while True:
             try:
@@ -41,10 +42,14 @@ class Matrix:
     def update(self):
         self.rows = len(self.matrix)
         self.columns = len(self.matrix[0])
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if self.matrix[i][j].is_integer():
+                    self.matrix[i][j] = int(self.matrix[i][j])
 
     def duplicate(self):
         self.update()
-        duplicate = Matrix(self.columns, self.rows)
+        duplicate = Matrix(self.rows, self.columns)
         for i in range(self.rows):
             for j in range(self.columns):
                 duplicate.matrix[i][j] = self.matrix[i][j]
@@ -75,34 +80,30 @@ class Matrix:
 
     def add(self, other):
         if isinstance(other, Matrix) and other.rows == self.rows and other.columns == self.columns:
-            result = Matrix(self.columns, self.rows)
+            result = Matrix(self.rows, self.columns)
             for row in self.matrix:
                 for column in range(self.columns):
                     result.matrix[row][column] = self.matrix[row][column] + other.matrix[row][column]
             return result
-        else:
-            return "These matrices are incompatible for addition."
         
     def multiply(self, other):
         if not isinstance(other, Matrix):
-            result = Matrix(self.columns, self.rows)
+            result = Matrix(self.rows, self.columns)
             for row in range(self.rows):
                 for num in range(self.columns):
                     result.matrix[row][num] = self.matrix[row][num]*other
             return result
 
         elif self.columns == other.rows:
-            result = Matrix(other.columns, self.rows)
+            result = Matrix(self.rows, other.columns)
             for i in range(self.rows):
                 for j in range(other.columns):
                     for k in range(other.rows):
                         result.matrix[i][j] += self.matrix[i][k] * other.matrix[k][j]
             return result
-        else:
-            return "Incompatible matrices."
         
     def transpose(self):
-        result = Matrix(self.rows, self.columns)
+        result = Matrix(self.columns, self.rows)
         for i in range(self.rows):
             for j in range(self.columns):
                 result.matrix[j][i] = self.matrix[i][j]
@@ -121,20 +122,20 @@ class Matrix:
     def get_determinant(self):
         matrix = self.matrix
         self.update()
-        if self.columns == self.rows == 2:
-            determminant = (matrix[0][0]*matrix[1][1]) - (matrix[1][0]*matrix[0][1])
-            return determminant
+        if self.columns == self.rows == 1:
+            return self.matrix[0][0]
+        elif self.columns == self.rows == 2:
+            determinant = (matrix[0][0]*matrix[1][1]) - (matrix[1][0]*matrix[0][1])
+            return determinant
         elif self.columns == self.rows:
             total = 0
             for i in range(self.columns):
                 a = matrix[0][i]
                 total += a * self.get_cofactor(0, i)
             return total
-        else:
-            return 'Matrix must be square.'
         
     def get_adjoint(self):
-        cofactor_matrix = Matrix(self.columns, self.rows)
+        cofactor_matrix = Matrix(self.rows, self.columns)
         for i in range(self.rows):
             for j in range(self.columns):
                 cofactor_matrix.matrix[i][j] = self.get_cofactor(i, j)
@@ -142,14 +143,16 @@ class Matrix:
         return adjoint_matrix
         
     def invert(self):
-        determinant = self.get_determinant()
-        if determinant != 0:
-            inverse = self.get_adjoint().multiply(1/determinant)
-            return inverse
-
-#sample code
-A = Matrix(3, 3)
-A.matrix = [[7, 4, 9], [8, -3, -6], [2, -1, -3]]
-B = Matrix(1, 3)
-B.matrix = [[0],[12],[5]]
-print(A.invert().multiply(B).matrix)
+        if self.rows == self.columns:
+            determinant = self.get_determinant()
+            if determinant != 0:
+                inverse = self.get_adjoint().multiply(1/determinant)
+                inverse.update()
+                return inverse
+    
+    def exponent(self, power):
+        if self.rows == self.columns:
+            result = self.duplicate()
+            for i in range(power-1):
+                result = self.multiply(result)
+            return result
